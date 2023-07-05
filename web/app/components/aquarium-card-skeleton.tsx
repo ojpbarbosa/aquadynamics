076fa1a9f978366991fcba256db81f863fd0bf2d
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-07-05 09:40:54 -0300

    chore(web): change aquarium card skeleton size

--- web/app/components/aquarium-card-skeleton.tsx
+++ web/app/components/aquarium-card-skeleton.tsx
@@ -1,7 +1,7 @@
 export default function AquariumCardSkeleton() {
   return (
-    <div className="h-60 rounded dark:text-neutral-100/80 animate-pulse text-neutral-900 border justify-around border-gray-300 dark:border-neutral-800 bg-neutral-100 dark:bg-neutral-800/30 flex flex-col items-start">
-      <div className="w-full"></div>
+    <div className="h-56 sm:h-60 rounded dark:text-neutral-100/80 animate-pulse text-neutral-900 border justify-around border-gray-300 dark:border-neutral-800 bg-neutral-100 dark:bg-neutral-800/30 flex flex-col items-start">
+      <div className="w-full" />
       <div className="border-t border-gray-300 dark:border-neutral-800 w-full p-4 h-10">
         <div className="h-4 bg-gray-200 block dark:bg-neutral-700 rounded-sm w-32" />
       </div>
