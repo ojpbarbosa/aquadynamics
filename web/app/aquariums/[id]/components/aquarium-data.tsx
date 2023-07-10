13062b87760af2ebc6bc9f83413523204478560f
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-07-10 00:33:26 -0300

    chore(web): change not found page sizing

--- web/app/aquariums/[id]/components/aquarium-data.tsx
+++ web/app/aquariums/[id]/components/aquarium-data.tsx
@@ -8,6 +8,7 @@ import Header from '@/components/layout/header'
 import { Aquarium } from '@/library/types'
 import AquariumControllerStatus from './aquarium-controller-status'
 import AquariumLog from './aquarium-log'
+import AquariumTemperatureChart from './aquarium-temperature-chart'
 
 type AquariumDataProps = {
   data: Aquarium
@@ -23,14 +24,24 @@ export default function AquariumData({ data }: AquariumDataProps) {
       <Header subtreeName={aquarium.name} subtreeUrl={pathname} />
       <main className="overflow-y-auto overflow-x-hidden h-[80vh] w-screen">
         <div className="flex w-screen h-full items-start justify-around">
-          <div className="w-5/6 flex flex-col sm:flex-row justify-between gap-4 py-10 sm:py-20">
-            <div className="aspect-video w-full sm:w-2/3 rounded border border-gray-300 dark:border-neutral-800 flex items-center justify-center bg-neutral-300/20 text-neutral-500 dark:text-neutral-500 dark:bg-neutral-800/30">
-              <PiSpinnerGapLight className="text-4xl animate-spin" />
+          <div className="w-5/6 flex flex-col sm:justify-between gap-4 py-10 sm:py-20 gap-y-4">
+            <div className="flex flex-row w-full">
+              <div className="aspect-video w-full sm:w-2/3 rounded border border-gray-300 dark:border-neutral-800 flex items-center justify-center bg-neutral-300/20 text-neutral-500 dark:text-neutral-500 dark:bg-neutral-800/30">
+                <PiSpinnerGapLight className="text-4xl animate-spin" />
+              </div>
+              <div className="flex flex-row sm:flex-col md:w-1/6 lg:w-1/3 gap-y-2 justify-between items-center">
+                <AquariumControllerStatus aquarium={aquarium} setAquarium={setAquarium} />
+                <AquariumLog aquarium={aquarium} setAquarium={setAquarium} />
+              </div>
             </div>
-            {/* <div className="flex flex-row sm:flex-col md:w-1/6 lg:w-1/3 gap-y-2 justify-between items-center">
-              <AquariumControllerStatus aquarium={aquarium} setAquarium={setAquarium} />
-              <AquariumLog aquarium={aquarium} setAquarium={setAquarium} />
-            </div> */}
+            <div>
+              <h1 className="text-2xl font-bold">Temperatura</h1>
+              <AquariumTemperatureChart
+                temperatures={aquarium.logs!.map((log) => {
+                  return { temperature: log.temperature, timestamp: log.timestamp }
+                })}
+              />
+            </div>
           </div>
         </div>
       </main>
---
+++ web/app/aquariums/[id]/components/aquarium-temperature-chart.tsx
@@ -0,0 +1,52 @@
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Label,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts'
+import ChartTooltip from './chart-tooltip'
+
+type AquariumTemperatureChartProps = {
+  temperatures: {
+    temperature: number
+    timestamp: Date
+  }[]
+}
+
+export default function AquariumTemperatureChart({ temperatures }: AquariumTemperatureChartProps) {
+  return (
+    <ResponsiveContainer width={'100%'} height={300} min-width={300}>
+      <AreaChart
+        data={temperatures}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 20,
+          bottom: 30
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="timestamp">
+          <Label value="Registro" position="bottom" />
+        </XAxis>
+        <YAxis dataKey="temperature">
+          <Label value="°C" position="left" angle={-90} dy="-10" />
+        </YAxis>
+        <Tooltip
+          content={
+            <ChartTooltip
+              term="Temperatura"
+              value={`${temperatures[0].temperature.toFixed(1).replace('.', ',')} °C`}
+              timestamp={new Date(temperatures[0].timestamp).toUTCString()}
+            />
+          }
+        />
+        <Area type="monotone" dataKey="temperature" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+    </ResponsiveContainer>
+  )
+}
