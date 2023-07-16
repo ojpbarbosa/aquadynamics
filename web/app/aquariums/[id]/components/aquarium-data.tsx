cb14820fd38c4d0b823cdf1429f133e435bbc3d8
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-07-15 21:30:45 -0300

    chore(web): add aquarium missing controller verification

--- web/app/aquariums/[id]/components/aquarium-data.tsx
+++ web/app/aquariums/[id]/components/aquarium-data.tsx
@@ -33,12 +33,14 @@ export default function AquariumData({ data }: AquariumDataProps) {
               <div className="aspect-video w-full sm:w-2/3 rounded border border-gray-300 dark:border-neutral-800 flex items-center justify-center bg-neutral-300/20 text-neutral-500 dark:text-neutral-500 dark:bg-neutral-800/30">
                 <PiSpinnerGapLight className="text-4xl animate-spin" />
               </div>
-              <div className="flex flex-row sm:flex-col md:w-1/6 lg:w-1/3 gap-y-2 justify-between items-center">
-                <AquariumControllerStatus aquarium={aquarium} setAquarium={setAquarium} />
-                {logs.length > 0 && (
-                  <AquariumLog aquariumId={aquarium.id} logs={logs} setLogs={setLogs} />
-                )}
-              </div>
+              {aquarium.controller && (
+                <div className="flex flex-row sm:flex-col md:w-1/6 lg:w-1/3 gap-y-2 justify-between items-center">
+                  <AquariumControllerStatus aquarium={aquarium} setAquarium={setAquarium} />
+                  {logs.length > 0 && (
+                    <AquariumLog aquariumId={aquarium.id} logs={logs} setLogs={setLogs} />
+                  )}
+                </div>
+              )}
             </div>
             {logs.length > 0 && (
               <div>
