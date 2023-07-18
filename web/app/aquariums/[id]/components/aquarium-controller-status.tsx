7f55a6cf1de8bf7f5511ddfa0e80d04fad64fcfa
João Pedro F. Barbosa <joaopedrofbarbosa@protonmail.com>
2023-07-18 19:24:41 -0300

    chore(web): change margin

--- web/app/aquariums/[id]/components/aquarium-controller-status.tsx
+++ web/app/aquariums/[id]/components/aquarium-controller-status.tsx
@@ -65,7 +65,7 @@ export default function AquariumControllerStatus({
             <PopoverTrigger>
               <FiHelpCircle className="text-neutral-400" />
             </PopoverTrigger>
-            <PopoverContent className="space-y-1 ml-7 bg-neutral-300/60 dark:bg-neutral-800/70 backdrop-blur filter dark:text-neutral-100 text-neutral-900 border-gray-300 dark:border-neutral-800 rounded">
+            <PopoverContent className="space-y-1 ml-8 bg-neutral-300/60 dark:bg-neutral-800/70 backdrop-blur filter dark:text-neutral-100 text-neutral-900 border-gray-300 dark:border-neutral-800 rounded">
               <p className="font-semibold">
                 Status do controlador<span className="text-neutral-400">{' – '} Legenda</span>
               </p>
