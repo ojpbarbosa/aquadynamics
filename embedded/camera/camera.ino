ddd34e89d56d940fdacb18d33b59e7bce01db62d
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-07-25 22:23:03 -0300

    chore(embedded/camera): change stream settings

--- embedded/camera/camera.ino
+++ embedded/camera/camera.ino
@@ -69,36 +69,13 @@ void setup_camera()
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_SXGA;
+  config.frame_size = FRAMESIZE_QVGA;
   config.pixel_format = PIXFORMAT_JPEG; // for streaming
-  // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-  // if PSRAM IC present, init with USXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if (config.pixel_format == PIXFORMAT_JPEG)
-  {
-    if (psramFound())
-    {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    }
-    else
-    {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SXGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  }
-  else
-  {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-  }
+  config.jpeg_quality = 23;
+  config.fb_count = 2;
+  config.grab_mode = CAMERA_GRAB_LATEST;
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
@@ -106,20 +83,6 @@ void setup_camera()
   {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
-  }
-
-  sensor_t *s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID)
-  {
-    s->set_vflip(s, 1);       // flip it back
-    s->set_brightness(s, 1);  // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  if (config.pixel_format == PIXFORMAT_JPEG)
-  {
-    s->set_framesize(s, FRAMESIZE_QVGA);
   }
 
 #if defined(LED_GPIO_NUM)
